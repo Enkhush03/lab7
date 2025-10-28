f2cbd69b4e5dccbeb0059020c0a3abe88f6e16c4
Enkhnasan03 <enkhush.c@gmail.com>
2025-10-28 12:07:12 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+class Shape{
+    protected:
+    string name;
+    public:
+     Shape(string n = "Shape") {
+        name = n;
+    }
+    void setName(string n) { name = n; }
+    string getName() { return name; }
+};
+class TwoDShape : public Shape {
+protected:
+    float area;
+    float perimeter;
+public:
+    TwoDShape(string n = "2D Shape") : Shape(n) {}
+    float getArea() { return area; }
+    float getPerimeter() { return perimeter; }
+};
+//triangle enkhush
+//circle nomio
+//square shagai
 
+int main(){
+
+}
