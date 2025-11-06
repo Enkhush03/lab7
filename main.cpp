f4cd9b58876faa1b4cc48403c449fd8e60f165b5
Enkhnasan03 <enkhush.c@gmail.com>
2025-11-06 18:20:43 +0800

    Virtual function

--- main.cpp
+++ main.cpp
@@ -16,106 +16,79 @@ class Shape{
     float getR(){ return r;}
 };
 class TwoDShape : public Shape {
-protected:
-    float area;
-    float perimeter;
 public:
+    virtual  float area()=0;
+    virtual  float perimeter()=0;
+    virtual void print() = 0; // виртуал функц
     TwoDShape(string n = "2D Shape", float rr = 0)
         : Shape(n, rr) {}  // эх классын байгуулагч рүү параметр дамжуулж байна
-    float getArea() { return area; }
-    float getPerimeter() { return perimeter; }
-    virtual void print() = 0; // виртуал функц
+    // float getArea() { return area; }
+    // float getPerimeter() { return perimeter; }
+   
     
 };
 //triangle enkhush
 class Triangle : public TwoDShape {
 private:
-    float x1, y1;
-    float x2, y2;
-    float x3, y3;
-
+    float x1, y1, x2, y2, x3, y3;
 public:
-    Triangle(float x, float y, float side, string n = "Triangle")
-        : TwoDShape(n, side) // эх классын Shape рүү side дамжуулж байна
-    {
-        x1 = x;
-        y1 = y;
+    Triangle(float x, float y, float side, string n = "Triangle") 
+        : TwoDShape(n, side) {
+        x1 = x; y1 = y;
         Vertices();
-        Area();
-        Perimeter();
     }
 
     void Vertices() {
-        float h = (sqrt(3) / 2) * r; // r-г эх клаассаас шууд ашиглаж болно
-        x2 = x1 - (r / 2);
-        y2 = y1 - h;
-        x3 = x1 + (r / 2);
-        y3 = y1 - h;
+        float h = (sqrt(3)/2) * r;
+        x2 = x1 - r/2; y2 = y1 - h;
+        x3 = x1 + r/2; y3 = y1 - h;
     }
 
-    void Area() {
-        area = (sqrt(3) / 4) * pow(r, 2);
-    }
+    float area() override { return (sqrt(3)/4) * pow(r, 2); }
+    float perimeter() override { return 3*r; }
 
-    void Perimeter() {
-        perimeter = 3 * r;
-    }
-
-    void print() {
+    void print() override {
         cout << "Shape: " << name << endl;
-        cout << "Vertices: " << endl;
-        cout << "  A(" << x1 << ", " << y1 << ")\n";
-        cout << "  B(" << x2 << ", " << y2 << ")\n";
-        cout << "  C(" << x3 << ", " << y3 << ")\n";
-        cout << "Area: " << area << ", Perimeter: " << perimeter << endl;
+        cout << "Vertices: A(" << x1 << "," << y1 << ") B(" << x2 << "," << y2 << ") C(" << x3 << "," << y3 << ")" << endl;
+        cout << "Area: " << area() << ", Perimeter: " << perimeter() << endl;
     }
 };
+
 class Circle: public TwoDShape {
 private:
-float x_center;
-float y_center;
+    float x_center, y_center;
 public:
-Circle(float x, float y, float radius, string n="Circle"): TwoDShape(n, radius){
-    x_center=x;
-    y_center=y;
-    calcArea();
-    calcPerimeter();
-}
-void calcArea(){
-    area= M_PI * r * r;
-}
-void calcPerimeter(){
-    perimeter= 2 * M_PI * r;
-}
-void print(){
-    cout<<"Shape: "<<name<<endl;
-    cout<<"Center: ("<<x_center<<", "<<y_center<<")"<<endl;
-    cout<<"Area: "<<area<<", Perimeter: "<<perimeter<<endl;
-}
+    Circle(float x, float y, float radius, string n="Circle"): TwoDShape(n, radius) {
+        x_center=x; y_center=y;
+    }
 
+    float area() override { return M_PI*r*r; }
+    float perimeter() override { return 2*M_PI*r; }
+
+    void print() override {
+        cout<<"Shape: "<<name<<endl;
+        cout<<"Center: ("<<x_center<<", "<<y_center<<")"<<endl;
+        cout<<"Area: "<<area()<<", Perimeter: "<<perimeter()<<endl;
+    }
 };
+
 //square shagai
 class Square : public TwoDShape {
 private:
     float x, y; // Зүүн дээд оройн координат
 public:
-    Square(float x0, float y0, float side, string n = "Square")
-        : TwoDShape(n, side), x(x0), y(y0) {
-        calcArea();
-        calcPerimeter();
-    }
-    void calcArea() {
-        area = r * r; // r-г эх классаас авна
-    }
-    void calcPerimeter() {
-        perimeter = 4 * r;
-    }
+    Square(float x0, float y0, float side, string n = "Square") : TwoDShape(n, side), x(x0), y(y0) {}
+
+    float area() override { return r*r; }
+    float perimeter() override { return 4*r; }
+
     void print() override {
         cout << "Shape: " << name << endl;
-        cout << "Top-left corner: (" << x << ", " << y << ")" << endl;
-        cout << "Area: " << area << ", Perimeter: " << perimeter << endl;
+        cout << "Top-left corner: (" << x << "," << y << ")" << endl;
+        cout << "Area: " << area() << ", Perimeter: " << perimeter() << endl;
     }
 };
+
 int main(){
  //нэгэн төрлийн классуудыг нэг эх класст объектод нь хийгээд дотор нь эрэмбэлсэн.
     TwoDShape *p[10];
@@ -131,7 +104,7 @@ int main(){
     int n=9;
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1; j++){
-            if(p[j]->getArea()>p[j+1]->getArea()){
+            if(p[j]->area()>p[j+1]->area()){
                 TwoDShape *t=p[j];
                 p[j]=p[j+1];
                 p[j+1]=t;
